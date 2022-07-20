ec6b42bcd1d11fac924bf75b1670da6d9ad218f6
rrrrrei <r-kishida5277@st.kyoto-art.ac.jp>
2022-07-21 06:48:35 +0900

    script修正

--- components/Main.js
+++ components/Main.js
@@ -2,7 +2,7 @@
 
 // 要素
 const Magic1 = new Audio('../assets/Magic1.mp3');
-const Magic2 = new Audio('../assets/Magic1.ogg');
+//const Magic2 = new Audio('../assets/Magic1.ogg');
 
 // 再生
 document.body.addEventListener('keydown',
@@ -12,7 +12,7 @@ event => {
        Magic1.currentTime = 0;
        Magic1.loop = true;
        Magic1.volume = 0.5;
-       Magic2.play();
+       //Magic2.play();
     }
 });
 

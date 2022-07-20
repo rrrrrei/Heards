9f66d18db42be070eba26ef56fb4fae99a51d166
rrrrrei <r-kishida5277@st.kyoto-art.ac.jp>
2022-07-21 06:01:29 +0900

    js変更

--- components/Main.js
+++ components/Main.js
@@ -3,14 +3,14 @@
 // 要素
 const Magic1 = new Audio('.assets/Magic1.mp3');
 const play = document.getElementById('play');
-Audio.currentTime = 0;
-Audio.loop = true;
 
 // 再生
 document.body.addEventListener('keydown',
 event => {
     if (event.key === 'v') {
        Magic1.play();
+       Magic1.currentTime = 0;
+       Magic1.loop = true;
     }
 });
 

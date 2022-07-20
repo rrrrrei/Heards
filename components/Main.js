c0ee812da0cf13a52590929a231d96614c35bc3c
rrrrrei <r-kishida5277@st.kyoto-art.ac.jp>
2022-07-21 05:52:10 +0900

    html削除、js追加

--- components/Main.js
+++ components/Main.js
@@ -1,17 +1,15 @@
-function keycodeOutput(e) {
-  const keyAction = document.querySelector(`.key[data-key="${e.key}"]`);
-  const audioAction = document.querySelector(`audio[data-key="${e.key}"]`);
+'use strict';
 
-  keyAction.classList.add('playing');
-  audioAction.play();
-}
-window.addEventListener('keydown', keycodeOutput);
+// 要素
+const Magic1 = new Audio('.assets/Magic1.mp3');
+const play = document.getElementById('play');
+Audio.currentTime = 0;
+Audio.loop = true;
 
-function keycodeRemove(e) {
-  const keyAction = document.querySelector(`.key[data-key="${e.key}"]`);
-  const audioAction = document.querySelector(`audio[data-key="${e.key}"]`);
-
-  keyAction.classList.remove('playing');
-  audioAction.pause();
-}
-window.addEventListener('keyup', keycodeRemove);
+// 再生
+document.body.addEventListener('keydown',
+event => {
+    if (event.key === 'v') {
+       Magic1.play();
+    }
+});
